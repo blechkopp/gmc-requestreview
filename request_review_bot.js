c62f6bdba505c09b99ca34e7fe2ef7bfd102c96f
blechkopp <36053234+blechkopp@users.noreply.github.com>
2023-07-11 07:32:20 +0200

    Add files via upload

---
+++ request_review_bot.js
@@ -0,0 +1,72 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var urls = document.getElementsByTagName('a');
+var counterAll = 0;
+var counterSuccess = 0;
+
+for (var i = 0; i < urls.length; i++) {
+  if(urls[i].getAttribute('href').includes('mc/items/details')) {
+    w = null;
+    xpath = null;
+    selectedNodeElements = null;
+    currentNode = null;
+    
+    counterAll++;
+    
+    try {
+      w = window.open(urls[i].getAttribute('href'), 'NewTab', '_blank');
+    } catch (error) { console.log ( 'window.open-Error '+error); }
+    
+    w.clickElem = () => {
+    const xpath = "//*[@id='products-root']/div/details-view/section[1]/issues/tableview/div/ess-table/ess-particle-table/div[1]/div/div[2]/div[2]/ess-cell[3]/product-issue-actions/div/reapproval-status/div";
+    const selectedNodeElements = w.document.evaluate(xpath, w.document, null, XPathResult.ANY_TYPE, null);
+    let currentNode = selectedNodeElements.iterateNext();
+    if (typeof currentNode !== "undefined") {
+      while (currentNode) {
+        console.log('Click 1');
+        currentNode.click();
+        break;
+        }
+      }
+    }
+    setTimeout(w.clickElem, 16000);
+    
+    w.clickElem = () => {
+    const xpath = "//*[@id='PRODUCTS-overlay-container']/div[1]/material-dialog/focus-trap/div[2]/div/div[2]/div[2]/material-checkbox/div[1]/material-ripple";
+    const selectedNodeElements = w.document.evaluate(xpath, w.document, null, XPathResult.ANY_TYPE, null);
+    let currentNode = selectedNodeElements.iterateNext();
+    if (typeof currentNode !== "undefined") {
+        while (currentNode) {
+        console.log('Click 2');
+        currentNode.click();
+        break;
+        }
+      }
+    }
+    setTimeout(w.clickElem, 18000);
+    
+    w.clickElem = () => {
+    const xpath = "//*[@id='PRODUCTS-overlay-container']/div[1]/material-dialog/focus-trap/div[2]/div/footer/div/material-yes-no-buttons/material-button[1]/material-ripple";
+    const selectedNodeElements = w.document.evaluate(xpath, w.document, null, XPathResult.ANY_TYPE, null);
+    let currentNode = selectedNodeElements.iterateNext();
+    if (typeof currentNode !== "undefined") {
+      while (currentNode) {
+        console.log('Click 3');
+        currentNode.click();
+        counterSuccess++;
+        break;
+        }
+      }
+    }
+    setTimeout(w.clickElem, 25000);
+    
+    console.log('start sleep');
+    await sleep(25000);
+    console.log(counterSuccess+' / '+counterAll+' succeeded');
+    console.log('stop sleep');
+  }
+}
+
+console.log('finish');
